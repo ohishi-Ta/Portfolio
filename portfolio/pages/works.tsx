bda54b2529ad06644efae76f825cc35fb39570d4
ohishi <lumiere.zzz03918@gmail.com>
2025-01-16 00:43:11 +0900

    更新

--- portfolio/pages/works.tsx
+++ portfolio/pages/works.tsx
@@ -9,26 +9,26 @@ import Image from "next/image";
 
 // microCMSへAPIリクエスト
 export const getStaticProps = async () => {
-  const blog = await client.get({ 
+  const blog = await client.get({
     endpoint: "blog",
     queries: {
       limit: 99,
-      filters:"cat[contains]WEBサイト",
-    }
+      filters: "cat[contains]WEBサイト",
+    },
   });
-  const blog_app = await client.get({ 
+  const blog_app = await client.get({
     endpoint: "blog",
     queries: {
       limit: 99,
-      filters:"cat[contains]WEBアプリ",
-    }
+      filters: "cat[contains]WEBアプリ",
+    },
   });
   const tag = await client.get({
-     endpoint: "tag",
-      queries: {
-      limit: 20 
-    }
-     });
+    endpoint: "tag",
+    queries: {
+      limit: 20,
+    },
+  });
   return {
     props: {
       blogs: blog.contents,
@@ -47,56 +47,18 @@ type Props = {
 
 const works: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   blogs,
-  blogs_app
+  blogs_app,
 }: Props) => {
-  console.log(blogs)
+  console.log(blogs);
   return (
     <Layout>
       <PageHeader pageTitle={"Works"} />
       <div className={style.page__inner}>
-        <h3 className="Title_2 en">Webサイト</h3>
-        <p>
-          有名大学の公式サイトや自治体の特設サイトなど立ち上げから数多く経験してきました。
-          <br />CMS構築では、Movable Type、Wordpressなどブロックエディタでお客様が簡単に更新できるようなシステムも実装しました。
-          <br />以下は、担当してきた主な実績です。
-        </p>
-        <p>
-
-        </p>
-
+        <h3 className="Title_2 en">自学</h3>
+        <div className={style.page__works_btn}>
+          <Link href="https://zenn.dev/t_oishi" target="_blank">Zenn</Link>
+        </div>
         <ul className={style.page__works_list}>
-          {blogs.map((blog) => (
-            <li className={style.page__works_item} key={blog.id}>
-              <Link href={`/works/${blog.id}`}>
-                <figure>
-                  {blog.image ? (
-                    <Image
-                      src={blog.image.url}
-                      width={blog.image.width}
-                      height={blog.image.height}
-                      alt="サムネイル"
-                    />
-                  ) : (
-                    <Image src="" alt="No Image" />
-                  )}
-                </figure>
-                {/* <p className={style.item_subtitle}>{blog.client}</p> */}
-                <p className={style.item_title}>{blog.title}</p>
-                <p className={style.item_skill}>
-                  {blog.tags.map((tag) => (
-                    <span key={tag.id}>{tag.tag}</span>
-                  ))}
-                </p>
-              </Link>
-            </li>
-          ))}
-        </ul>
-
-        <div className={style.page__block}>
-          <h3 className="Title_2 en">自学</h3>
-          <p>Udemyなどで勉強しながら、開発しました。</p>
-
-          <ul className={style.page__works_list}>
           {blogs_app.map((blog) => (
             <li className={style.page__works_item} key={blog.id}>
               <Link href={`/works/${blog.id}`}>
@@ -122,6 +84,37 @@ const works: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
               </Link>
             </li>
           ))}
+        </ul>
+
+        <div className={style.page__block}>
+          <h3 className="Title_2 en">Webサイト</h3>
+
+          <ul className={style.page__works_list}>
+            {blogs.map((blog) => (
+              <li className={style.page__works_item} key={blog.id}>
+                <Link href={`/works/${blog.id}`}>
+                  <figure>
+                    {blog.image ? (
+                      <Image
+                        src={blog.image.url}
+                        width={blog.image.width}
+                        height={blog.image.height}
+                        alt="サムネイル"
+                      />
+                    ) : (
+                      <Image src="" alt="No Image" />
+                    )}
+                  </figure>
+                  {/* <p className={style.item_subtitle}>{blog.client}</p> */}
+                  <p className={style.item_title}>{blog.title}</p>
+                  <p className={style.item_skill}>
+                    {blog.tags.map((tag) => (
+                      <span key={tag.id}>{tag.tag}</span>
+                    ))}
+                  </p>
+                </Link>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
